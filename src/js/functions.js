d082c1d870ab61a3adf19f0df344bac2e097282e
YuriyOhii <uaogiy@gmail.com>
2023-07-02 15:28:55 +0300

    adds loader, error and css

--- src/js/functions.js
+++ src/js/functions.js
@@ -9,13 +9,19 @@ function fillInBreeds(data) {
 
 function renderCatCard(data) {
   const cat = data[0].breeds[0];
-  console.dir(cat);
-  const catCardMarkup = `<img class="card-img" src="${data[0].url}" alt="Cat of ${cat.name} breed" width="480">
-  <h2 class="card-title">${cat.name}</h2>
-  <p class="cat-description">${cat.description}</p>
-  <p class="cat-temperament">${cat.temperament}</p>`;
+  const catCardMarkup = `<img class="card-img item" src="${data[0].url}" alt="Cat of ${cat.name} breed" width="480">
+ <div class="text-wrap item"> <h2 class="card-title">${cat.name}</h2>
+  <p class="cat-description"><b>Description: </b>${cat.description}</p>
+  <p class="cat-temperament"><b>Temperament: </b>${cat.temperament}</p></div>`;
 
   refs.cardContainer.innerHTML = catCardMarkup;
+};
+
+function showErrorMessage(er) {
+  refs.select.classList.add('is-hidden');
+  refs.loader.classList.add('is-hidden');
+  refs.errorMessage.insertAdjacentHTML('beforeend',` ${er}`);
+  refs.errorMessage.classList.remove('is-hidden');
 }
 
-export { fillInBreeds, renderCatCard };
+export { fillInBreeds, renderCatCard, showErrorMessage };
--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,32 @@
-import "../css/common.css";
-import refs from "./refs.js";
-import { Cat_API } from "./cat-api.js";
-import { fillInBreeds, renderCatCard } from "./functions.js";
+import '../css/common.css';
+import refs from './refs.js';
+import { Cat_API } from './cat-api.js';
+import { fillInBreeds, renderCatCard, showErrorMessage } from './functions.js';
 
 const cat_API = new Cat_API();
-cat_API.fetchBreeds().then(data => {
-    fillInBreeds(data);
-    refs.loader.classList.add('is-hidden');
-})
 
 refs.select.addEventListener('change', onSelectCheck);
 
-function onSelectCheck({target}) {
-    cat_API.catBreed = target.value;
-    cat_API.fetchCatByBreed().then(renderCatCard)
+cat_API
+  .fetchBreeds()
+  .then(data => {
+    fillInBreeds(data);
+    refs.select.classList.remove('is-hidden');
+  })
+  .catch(showErrorMessage)
+
+  .finally(refs.loader.classList.add('is-hidden'));
+
+function onSelectCheck({ target }) {
+  refs.cardContainer.innerHTML = '';
+  refs.loader.classList.remove('is-hidden');
+  cat_API.catBreed = target.value;
+
+  cat_API
+    .fetchCatByBreed()
+    .then(data => {
+      renderCatCard(data);
+      refs.loader.classList.add('is-hidden');
+    })
+    .catch(showErrorMessage);
 }
